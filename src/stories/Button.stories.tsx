ee9b7fe7f24d72ee060ff3b094e289197bd25fef
ddfilipov <denisfilipov12+3@gmail.com>
2021-09-15 08:52:04 +0200

    applied args in a component level. That way I have to type less code. In order to add args in the Meta default component the tsconfig.json has to be in strict:true or strictBindCallApply:true

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -3,7 +3,11 @@ import { Button, ButtonProps } from './Button';
 
 export default {
   title: 'Components/MyButton',
-  component: Button
+  component: Button,
+  args: {
+    primary: true,
+    label: "Hello World"
+  },
 } as Meta
 
 
@@ -11,26 +15,19 @@ export default {
 const Template: Story<ButtonProps> = (args) => <Button {...args}/>
 
 
-export const Primary =  Template.bind({}) 
-Primary.args = {
-  primary: true,
-  label: "Hello World",
-}
+export const Primary =  Template.bind({})
 
 export const Secondary = Template.bind({})
 Secondary.args = {
-  ...Primary.args,
   primary: false,
 }
 
 export const Small = Template.bind({})
 Small.args = {
-  ...Primary.args,
   size: "small",
 }
 
 export const Large = Template.bind({})
 Large.args = {
-  ...Primary.args,
   size: "large",
 }
